ba5d6721190d303994d810fb4daf1632f840fcc7
ShariarShapnil <103377996+ShariarShapnil@users.noreply.github.com>
2023-08-25 21:23:33 +0600

    Delete widgets directory

--- widgets/colors.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppColors {
-  static const primaryColor = Color(0xff1f2d42);
-  static const boxColor = Color(0xff2b405e);
-  static const buttonColor = Color(0xff486da3);
-}
